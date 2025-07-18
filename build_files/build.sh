0f923ddeb52ea77603d91affea306baa0a5f0bd3
DXC-0 <62696671+DXC-0@users.noreply.github.com>
2025-07-18 08:07:03 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,8 +1,27 @@
-#!/bin/bash
+#!/bin/bashhttps://github.com/DXC-0/atomic-burritos/blob/main/build_files/build.sh
 
 set -ouex pipefail
 
+### Nvidia Open Installation
 
+--from=ghcr.io/ublue-os/akmods-nvidia-open:main-42 /rpms/ /tmp/rpms
+find /tmp/rpms
+rpm-ostree install /tmp/rpms/ublue-os/ublue-os-nvidia*.rpm
+sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/nvidia-container-toolkit.repo
+sed -i '0,/enabled=0/{s/enabled=0/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-nvidia.repo 
+rpm-ostree install /tmp/rpms/kmods/kmod-nvidia*.rpm libnvidia-fbc libva-nvidia-driver nvidia-driver nvidia-driver-cuda nvidia-modprobe nvidia-persistenced nvidia-settings nvidia-container-toolkit
+semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
+
+dnf5 -y copr enable ublue-os/packages
+dnf5 -y copr enable ublue-os/staging
+dnf5 -y config-manager setopt "*akmods*".priority=1
+dnf5 -y config-manager setopt "*staging*".exclude="scx-scheds kf6-* mesa* mutter* rpm-ostree* systemd* gnome-shell gnome-settings-daemon gnome-control-center gnome-software libadwaita tuned*"
+
+
+### Proprietary packages : 
+
+curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
+sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
 ### Install packages
 
@@ -10,6 +29,7 @@ dnf5 install -y sddm
 dnf5 install -y sway
 dnf5 install -y waybar
 dnf5 install -y SwayNotificationCenter
+dnf5 install -y wofi
 dnf5 install -y flatpak
 
 ### Dev Tooling installation
